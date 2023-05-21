1962729c9fe19d6a8ac567e6f569f3dbed6ee405
Rex <vivelarepublique@qq.com>
2023-05-22 02:14:57 +0800

    class name update

--- index.js
+++ index.js
@@ -16,26 +16,46 @@ const cssContent = paragraph.map(el => {
 if (argv?.[2]) {
     const html = readFileSync(argv[2], { encoding: 'utf-8' });
     const body = html.match(/(?<=<body>)([\s\S]*)(?=<\/body>)/i)?.[0];
+
+    //elements
     const elements = body.match(/(?<=<)\w+(?=\s)/g);
     const elementArray = Array.from(new Set(elements));
 
-    const result = [];
+    const step1Result = [];
 
     elementArray.forEach(el => {
         const elRegex1 = new RegExp(`(?<!-)${el}\\s`, 'g');
         const elRegex2 = new RegExp(`(?<!-)${el},`, 'g');
         cssContent.forEach(({ selector, content }) => {
             if (elRegex1.test(selector) || elRegex2.test(selector)) {
-                result.push(content);
+                step1Result.push(content);
             }
         });
     });
 
-    const minCss = result.reduce((acc, cur) => acc + '\n\n' + cur, '');
+    //class names
+    const classNames = body.match(/(?<=class=").+?(?=")/g);
+
+    const classNameArray = Array.from(new Set(Array.from(classNames).reduce((acc, cur) => acc.concat(cur.split(' ')), [])));
+
+    const step2Result = [];
+
+    classNameArray.forEach(el => {
+        const elRegex1 = new RegExp(`(?<=\.)${el}\\s`, 'g');
+        const elRegex2 = new RegExp(`(?<=\.)${el}\:`, 'g');
+        const elRegex3 = new RegExp(`(?<=\.)${el}\.`, 'g');
+        cssContent.forEach(({ selector, content }) => {
+            if (elRegex1.test(selector) || elRegex2.test(selector) || elRegex3.test(selector)) {
+                step2Result.push(content);
+            }
+        });
+    });
+
+    console.log(elementArray, '\n', classNameArray);
+
+    const minCss = Array.from(new Set([...step1Result, ...step2Result])).reduce((acc, cur) => acc + '\n\n' + cur, '');
 
     writeFileSync(join('test/', 'min.css'), minCss);
-
-    console.log(elementArray);
 }
 
 // console.log('\x1b[31mRed text\x1b[0m');
